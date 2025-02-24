470af466e9a295e4546f4b0c528d04ac6cf2b4ac
Vishal <vishu.nick@gmail.com>
2025-02-25 00:27:03 +0530

    Added delimeter check and number parse test and code

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,11 +5,25 @@ RSpec.describe 'String Calculator' do
         expect(add("")).to eq(0)
     end
 
-    it 'extracts and return delimeters from numbers' do
-        expect(extract_delimiter("1,2,3")).to eq([/,|\n/, "1,2,3"])
+    describe 'manages delimeter' do
+        it 'returns default delimiters (comma and newline) if no custom delimiter is provided' do
+            delimiter, numbers = extract_delimiter("1,2,3")
+            expect(delimiter).to eq(/,|\n/)
+            expect(numbers).to eq("1,2,3")
+        end
+
+        it 'extracts a single-character custom delimiter' do
+            delimiter, numbers = extract_delimiter("//;\n1;2;3")
+            expect(delimiter).to eq(";")
+            expect(numbers).to eq("1;2;3")
+        end        
     end
 
-    it 'extracts and return delimeters from numbers' do
-        expect(extract_delimiter("//;\n1;2;3")).to eq([";", "1;2;3"])
+    describe 'parses input numbers' do
+        it 'extracts a single-character custom delimiter' do
+            delimiter, numbers = extract_delimiter("//;\n1;2;3")
+            num_list = parse_numbers(delimiter, numbers)
+            expect(num_list).to eq([1,2,3])
+        end        
     end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,11 +1,18 @@
 def add(numbers)
     return 0 if numbers.empty?
+
+    delimiter, numbers = extract_delimiter(numbers)
+    num_list = parse_numbers(numbers, delimiter)
 end
 
 def extract_delimiter(numbers)
     if numbers.start_with?("//")
       parts = numbers.split("\n", 2)
-      return Regexp.escape(parts[0][2..-1]), parts[1] # Fixed slicing
+      return Regexp.escape(parts[0][2..-1]), parts[1]
     end
     [/,|\n/, numbers]
+end
+
+def parse_numbers(delimiter,numbers)
+    numbers.split(Regexp.new(delimiter)).map(&:to_i)
 end
