c57e219365d13c81a2ea8e335e0a76c56fbf0638
Vishal <vishu.nick@gmail.com>
2025-02-24 20:44:55 +0530

    Added delimeter handling logic

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'String Calculator' do
         expect(add("")).to eq(0)
     end
 
-    it 'returns the sum of comma-separated numbers' do
-        expect(add("1,2,3")).to eq(6)
+    it 'extracts and return delimeters from numbers' do
+        expect(extract_delimiter("1,2,3")).to eq([/,|\n/, "1,2,3"])
     end
-    
-    it 'handles newlines as delimiters' do
-    expect(add("1\n2,3")).to eq(6)
+
+    it 'extracts and return delimeters from numbers' do
+        expect(extract_delimiter("//;\n1;2;3")).to eq([";", "1;2;3"])
     end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,3 +1,11 @@
 def add(numbers)
     return 0 if numbers.empty?
 end
+
+def extract_delimiter(numbers)
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      return Regexp.escape(parts[0][2..-1]), parts[1] # Fixed slicing
+    end
+    [/,|\n/, numbers]
+end
